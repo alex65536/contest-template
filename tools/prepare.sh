1a07e31741ed6c7d6d7657424eb206ffb707c414
Alexander Kernozhitsky <sh200105@mail.ru>
2018-06-08 21:22:36 +0300

    Create problem-list.txt even if there are no problems

--- tools/prepare.sh
+++ tools/prepare.sh
@@ -7,6 +7,8 @@ function processDir {
 cd problems || exit 1
 rm -f ../problem-list.txt
 
+touch ../problem-list.txt
+
 for TASK_NAME in $(ls . | sort); do
 	if [[ -d "${TASK_NAME}" ]]; then
 		[[ -f "${TASK_NAME}/.prp-ignore" ]] && continue
