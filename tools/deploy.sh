095f5fb58af8145613aa04b6ba387d6ee0da11e7
Alexander Kernozhitsky <sh200105@mail.ru>
2018-06-12 12:00:39 +0300

    Excluding archives

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -5,7 +5,7 @@ CONTEST_NAME="$(basename "${PWD}")"
 SSH_HOST="${CMS_CONTEST_DEPLOY_USER}@${CMS_CONTEST_DEPLOY_HOST}"
 SSH_PATH="${CMS_CONTEST_DEPLOY_DIR}${CONTEST_NAME}"
 
-rsync -trl --del --progress "${PWD}/" "${SSH_HOST}:${SSH_PATH}/"
+rsync -trl --del --progress "${PWD}/" "${SSH_HOST}:${SSH_PATH}/" --exclude 'archives/*'
 
 ssh -t "${SSH_HOST}" "bash -c \"
     . ~/cmsvenv.sh
