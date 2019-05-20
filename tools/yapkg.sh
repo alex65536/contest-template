a5b6e7fc7d97fa605e4f79606559c058246b4688
Alexander Kernozhitsky <sh200105@mail.ru>
2019-05-21 01:04:15 +0300

    Upgrade yapkg

--- tools/yapkg.sh
+++ tools/yapkg.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-if [[  $# != 1 ]]; then
+if [[ $# != 1 ]]; then
 	echo "Usage yapkg.sh problem"
+	exit 1
 fi
+
+make prepare
+(
+	cd problems/"$1" || exit
+	make
+)
 
 tools/ya_prepare.sh normal
 (
-	cd problems/"$1"
+	cd problems/"$1" || exit
 	rm ../"$1.zip"
 	zip -r ../"$1.zip" *
 )
